fc188ed6f33e3934236e574b57195333a502bfde
Tom Wentworth <twentworth12@gmail.com>
2017-12-15 12:53:04 -0500

    SF

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ var conn = new jsforce.Connection({
   // you can change loginUrl to connect to sandbox or prerelease env.
   // loginUrl : 'https://test.salesforce.com'
 });
-conn.login(username, password, function(err, userInfo) {
+conn.login(SF_USER, SF_PASS, function(err, userInfo) {
   if (err) { return console.error(err); }
   // Now you can get the access token and instance URL information.
   // Save them to establish connection next time.
