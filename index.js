134f50902e5b91593659366622d0fdddf18ee32a
Tom Wentworth <twentworth12@gmail.com>
2017-12-15 12:03:51 -0500

    sf

--- index.js
+++ index.js
@@ -89,6 +89,27 @@ function querySalesforce(emailAddress, callbackFn, conversationId, orgId) {
 		app.get('/oauth2/auth', function(req, res) {
 		  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
 		});
+		
+		//
+		// Pass received authorization code and get access token
+		//
+		app.get('/oauth2/callback', function(req, res) {
+		  var conn = new jsforce.Connection({ oauth2 : oauth2 });
+		  var code = req.param('code');
+		  conn.authorize(code, function(err, userInfo) {
+			if (err) { return console.error(err); }
+			// Now you can get the access token, refresh token, and instance URL information.
+			// Save them to establish connection next time.
+			console.log(conn.accessToken);
+			console.log(conn.refreshToken);
+			console.log(conn.instanceUrl);
+			console.log("User ID: " + userInfo.id);
+			console.log("Org ID: " + userInfo.organizationId);
+			// ...
+			res.send('success'); // or your desired response
+		  });
+		});
+		
 
 		var records = [];
 		
