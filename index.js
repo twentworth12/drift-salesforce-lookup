3a42254ab3c80c60c248c66454e72204d862c6bf
Tom Wentworth <twentworth12@gmail.com>
2017-12-18 13:32:24 -0500

    Bug fix

--- index.js
+++ index.js
@@ -142,8 +142,7 @@ function querySalesforce(emailAddress, accessToken, callbackFn, conversationId, 
 		  
   
 		  // Built the Drift reply body
-		  body = "Lookup Results (Note an empty response means we don't have an email address or the user isn't in Salesforce yet<br/><a target='_blank' href=https://na52.salesforce.com/" + Id + ">" + firstName + " " + lastName + "</a> | " + Company + " | " + Country + "<br/>Total RM Studio Starts: " + totalStudioStarts + " | Last RM Studio Usage: " + lastStudioUsage + "<br/>Academic: " + Academic
-		  console.log ("about to send message")
+		  body = "<a target='_blank' href=https://na52.salesforce.com/" + Id + ">" + firstName + " " + lastName + "</a> | " + Company + " | " + Country + "<br/>Total RM Studio Starts: " + totalStudioStarts + " | Last RM Studio Usage: " + lastStudioUsage + "<br/>Academic: " + Academic
 		  callbackFn(body, conversationId, orgId)
 		  return
 		     }); 
