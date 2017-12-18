2cf0cb83136c831af95fb80b2faf8d3e62da8f6d
Tom Wentworth <twentworth12@gmail.com>
2017-12-18 13:02:34 -0500

    sdaf

--- index.js
+++ index.js
@@ -145,10 +145,14 @@ function querySalesforce(emailAddress, accessToken, callbackFn, conversationId, 
 		  body = "<a target='_blank' href=https://na52.salesforce.com/" + Id + ">" + firstName + " " + lastName + "</a> | " + Company + " | " + Country + "<br/>Total RM Studio Starts: " + totalStudioStarts + " | Last RM Studio Usage: " + lastStudioUsage + "<br/>Academic: " + Academic
 		  console.log ("about to send message")
 		  callbackFn(body, conversationId, orgId)
-		     }); 
+		     }
+			
+			console.log ("after the message should be sent " + conn.result + " is the result")
+	     
+		     
+		     ); 
 		
 		 
-		console.log ("after the message should be sent " + conn.result + " is the result")
 
 			
 
